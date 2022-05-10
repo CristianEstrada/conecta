33d793fb4a1425646985e9535cb76cd8e0e67274
Cristian Rodrigo Estrada Romero <ecristian963@gmail.com>
2022-05-10 14:40:54 -0500

    Goggle Asycn
    add google services
    update and refactoring auth provider

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -5,17 +5,27 @@ import * as Google from 'expo-google-app-auth';
 
 const AuthContext = createContext({});
 
+const config = {
+  androidClientId: "731900721296-me1r4tbjqbu1ptrlo3c088dg10gauupd.apps.googleusercontent.com",
+  iosClientId: "731900721296-caimt3qept7ehel9t8hg22f7bnctnfnk.apps.googleusercontent.com",
+  scopes: ["profile", "email"],
+  Permissions: ["public_profile", "email", "gender", "location"],
+}
+
 export const AuthProvider = ({ children }) => {
 
 
     const signInWithGoogle = async () => {
-        await Google.logInAsync()
-    }
+        await Google.logInAsync(config).then(async (logInResult) => { if(logInResult.type === "success") {
+
+        }});
+    };
 
   return (
     <AuthContext.Provider
       value={{
         user: null,
+        signInWithGoogle,
       }}
     >
       {children}
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native'
+import { Button,View, Text } from 'react-native'
 import React from 'react'
+import useAuth from '../hooks/useAuth'
 
 
 const LoginScreen = () => {
+  const { signInWithGoogle } = useAuth();
   return (
     <View>
       <Text>Inicio de Sesion</Text>
+      <Button title='Login' onPress={signInWithGoogle} />
     </View>
   )
 }
